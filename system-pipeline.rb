dc39ae383cfadee8600c5d373f3860ce47ba6ba9
davidmz <david.mazvovsky@gmail.com>
2013-01-24 22:44:20 +0200

    added logging

--- system-pipeline.rb
+++ system-pipeline.rb
@@ -1,25 +1,35 @@
 require 'rubygems'
 require 'active_record'
 require 'active_support/all'
+require 'logging'
 
 ActiveRecord::Base.establish_connection(
-    :adapter  => 'mysql2',
+    :adapter => 'mysql2',
     :database => 'jdeering_centracrm',
     :username => 'jdeering_david',
     :password => '0953acb',
-    :host     => 'centracorporation.com')
+    :host => 'centracorporation.com')
 
 class Lead < ActiveRecord::Base
-
 end
 
-week_old_leads = Lead.where('date_entered < ? and status= ?', 7.day.ago,'FU')
+logger = Logging.logger['logger']
+logger.add_appenders(
+    Logging.appenders.stdout,
+    Logging.appenders.file('centra.log')
+)
+
+logger.level = :info
+
+week_old_leads = Lead.where('date_entered < ? and status= ?', 7.day.ago, 'FU')
+
+logger.info "We have found #{week_old_leads.to_s} leads"
 
 week_old_leads.each do |lead|
   lead.status = 'SP'
-  lead.assigned_user_id =  '92b0bdb7-bb6c-449f-fa73-510054673707'
+  lead.assigned_user_id = '92b0bdb7-bb6c-449f-fa73-510054673707'
   lead.save
 end
 
-puts "we have modifued: " + week_old_leads.length.to_s + " leads"
+logger.info "we have modified:  #{week_old_leads.length.to_s} leads"
 
