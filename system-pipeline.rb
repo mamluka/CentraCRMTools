ee83c6bbabfd893962a74865ba3ba614207b9b8e
davidmz <david.mazvovsky@gmail.com>
2013-01-25 01:22:15 +0200

    added the emailing to the system pipline trasform code

--- system-pipeline.rb
+++ system-pipeline.rb
@@ -10,9 +10,6 @@ ActiveRecord::Base.establish_connection(
     :password => '0953acb',
     :host => 'centracorporation.com')
 
-class Lead < ActiveRecord::Base
-end
-
 logger = Logging.logger['logger']
 logger.add_appenders(
     Logging.appenders.stdout,
@@ -21,6 +18,23 @@ logger.add_appenders(
 
 logger.level = :info
 
+class Lead < ActiveRecord::Base
+end
+
+class LeadsCustomData < ActiveRecord::Base
+  set_table_name 'leads_cstm'
+  set_primary_key  'id_c'
+end
+
+
+class EmailAddress < ActiveRecord::Base
+  set_table_name 'email_addresses'
+end
+
+class EmailAddressRelation < ActiveRecord::Base
+  set_table_name 'email_addr_bean_rel'
+end
+
 week_old_leads = Lead.where('date_entered < ? and status= ?', 7.day.ago, 'FU')
 
 logger.info "We have found #{week_old_leads.to_s} leads"
@@ -28,8 +42,33 @@ logger.info "We have found #{week_old_leads.to_s} leads"
 week_old_leads.each do |lead|
   lead.status = 'SP'
   lead.assigned_user_id = '92b0bdb7-bb6c-449f-fa73-510054673707'
+
   lead.save
+
+  if custom_data.prev_url_c != "http://" && EmailAddressRelation.exists?(:bean_id => lead.id)
+    logger.info "loaded custom data for #{lead.first_name} the data has in it #{custom_data.prev_url_c}"
+
+    rel = EmailAddressRelation.where(:bean_id => lead.id).first
+    email = EmailAddress.find(rel.email_address_id)
+
+    logger.info "it has the email: #{email.email_address}"
+
+    res = RestClient.get 'http://apps.centracorporation.com/api/email/first-system-pipeline', {:params=> { :email => email.email_address,:previewUrl =>  custom_data.prev_url_c}}
+
+    logger.info "The api call response: #{res}"
+
+    if res == "OK"
+      custom_data.system_pipeline_email_1_c = Time.now
+      custom_data.save
+
+      logger.info "Saved the new status"
+    end
+
+    sleep 3
+
+    logger.info "--Done--"
+  end
 end
 
-logger.info "we have modified:  #{week_old_leads.length.to_s} leads"
+logger.info "we have modified and sent to:  #{week_old_leads.length.to_s} leads"
 
