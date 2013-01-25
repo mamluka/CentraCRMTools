45b1396c3db92696befb267fe19df98e55e0fc76
davidmz <david.mazvovsky@gmail.com>
2013-01-25 13:36:52 +0200

    lucky

--- active_records_models.rb
+++ active_records_models.rb
@@ -1,5 +1,8 @@
 class Lead < ActiveRecord::Base
+  self.primary_key = 'id'
   has_one :leads_custom_data, :primary_key => 'id', :foreign_key => 'id_c'
+
+  has_many :email_address, :through => :email_address_relation
 end
 
 class LeadsCustomData < ActiveRecord::Base
@@ -10,8 +13,13 @@ end
 
 class EmailAddress < ActiveRecord::Base
   self.table_name = 'email_addresses'
+  self.primary_key = 'id'
 end
 
 class EmailAddressRelation < ActiveRecord::Base
   self.table_name = 'email_addr_bean_rel'
+  self.primary_key = 'id'
+
+  belongs_to :lead, :primary_key => 'id', :foreign_key => 'bean_id'
+  belongs_to :email_address, :primary_key => 'id', :foreign_key => 'email_address_id'
 end
