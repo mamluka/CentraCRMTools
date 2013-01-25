862baa1e17910f2a0f50407a7ab37208e153b1fd
root <root@server3.anydomain.com>
2013-01-25 15:50:01 +0400

    fone

--- testing.rb
+++ testing.rb
@@ -5,4 +5,4 @@ lead = Lead.find('499eab2d-e0df-b699-17da-50fdebe2d35d')
 
 puts lead.id
 puts lead.leads_custom_data.company_name_c
-puts lead.email_address_relation.email_address_id
+puts lead.email_addresss.first.email_address
