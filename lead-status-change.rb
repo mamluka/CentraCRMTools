97ca28cbe384d9e4da4b88aaa4f92783e688eea2
davidmz <david.mazvovsky@gmail.com>
2013-01-28 20:23:28 +0200

    more

--- lead-status-change.rb
+++ lead-status-change.rb
@@ -4,6 +4,6 @@ load 'active_records_models.rb'
 current_status = ARGV[0]
 desired_status = ARGV[1]
 
-leads_with_current_status = Lead.find(:status=> current_status)
+leads_with_current_status = Lead.where(:status=> current_status)
 
 puts "Found #{leads_with_current_status.length.to_s} leads with status #{current_status}"
