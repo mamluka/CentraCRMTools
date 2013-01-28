27f715174c755315903969f183e774e4db384090
davidmz <david.mazvovsky@gmail.com>
2013-01-28 20:27:40 +0200

    status mess update

--- lead-status-change.rb
+++ lead-status-change.rb
@@ -7,3 +7,12 @@ desired_status = ARGV[1]
 leads_with_current_status = Lead.where(:status=> current_status)
 
 puts "Found #{leads_with_current_status.length.to_s} leads with status #{current_status}"
+
+if not desired_status.nil?
+  leads_with_current_status.each do |lead|
+    lead.status = desired_status
+    lead.save
+  end
+end
+
+puts "Moved #{leads_with_current_status.length.to_s} leads with status #{current_status} to status #{desired_status}"
