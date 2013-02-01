9882e6e63a2d7940a82a3d5e7daa9fc4d8c9df76
davidmz <david.mazvovsky@gmail.com>
2013-02-01 23:50:56 +0200

    some order

--- testing.rb
+++
@@ -1,8 +0,0 @@
-load 'init.rb'
-load 'active_records_models.rb'
-
-lead = Lead.find('499eab2d-e0df-b699-17da-50fdebe2d35d')
-
-puts lead.id
-puts lead.leads_custom_data.company_name_c
-puts lead.email_addresss.first.email_address
