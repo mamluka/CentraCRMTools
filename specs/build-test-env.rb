77d95f2ef75d022782af639db38f59f5234a06ed
davidmz <david.mazvovsky@gmail.com>
2013-02-14 21:04:03 +0200

    yeah

--- specs/build-test-env.rb
+++
@@ -1,8 +0,0 @@
-require 'mysql2'
-
-client = Mysql2::Client.new(:database => 'jdeering_centracrm',
-                            :username => 'jdeering_david',
-                            :password => '0953acb',
-                            :host => 'centracorporation.com')
-
-puts client.query('select * from leads')
