74e77ebc6dae37b24a744eb3f87ba8e3395cbda5
davidmz <david.mazvovsky@gmail.com>
2013-02-14 23:37:31 +0200

    fix

--- specs/test/base/auth.rb
+++ specs/test/base/auth.rb
@@ -7,11 +7,14 @@ class Auth
     @driver = driver
   end
 
+
   def login(username = nil, password = nil)
     config = JSON.parse(File.read(@config))
 
     username ||= config['admin_username']
     password ||= config['admin_password']
+
+    @driver.goto 'crmtesting.centracorporation.com'
 
     @driver.text_field(:name => 'user_name').set username
     @driver.text_field(:name => 'user_password').set password
