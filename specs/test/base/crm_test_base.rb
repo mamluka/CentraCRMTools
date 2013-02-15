7ef47027a31bf849b462e06008ced2b8c3a926b2
davidmz <david.mazvovsky@gmail.com>
2013-02-15 19:06:17 +0200

    yeah

--- specs/test/base/crm_test_base.rb
+++ specs/test/base/crm_test_base.rb
@@ -2,7 +2,7 @@ require 'json'
 
 class CrmTestBase < MiniTest::Unit::TestCase
   def assert_api_called(params)
-    actual_api_call = JSON.parse(file.read('api-call.json'))
+    actual_api_call = JSON.parse(File.read('api-call.json'))
 
     params.each do |k, v|
       assert actual_api_call.has_key?(k.to_s) && actual_api_call[k.to_s] == v
