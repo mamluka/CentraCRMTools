9880c186c37da3cd98a73f914870f1c7861b16bb
davidmz <david.mazvovsky@gmail.com>
2013-02-16 17:18:07 +0200

    added rake

--- specs/crm/base/api-interceptor.rb
+++ specs/crm/base/api-interceptor.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 require 'json'
 
 get '/api/email/*' do
-  File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
+  File.open('/tmp/api-call.json', 'w') { |file| file.write(params.to_json) }
   'OK'
 end
--- specs/crm/base/crm_test_base.rb
+++ specs/crm/base/crm_test_base.rb
@@ -8,18 +8,18 @@ class CrmTestBase < MiniTest::Unit::TestCase
     @auth = Auth.new @driver
     @auth.login
 
-    `screen -L -dmS api ruby base/api-interceptor.rb`
+    `screen -L -dmS api ruby #{File.dirname(__FILE__)}/base/api-interceptor.rb`
   end
 
   def teardown
     @auth.logout
 
     `pkill -f api-interceptor.rb`
-    `rm -rf api-call.json`
+    `rm -rf /tmp/api-call.json`
   end
 
   def assert_api_called(params)
-    actual_api_call = JSON.parse(File.read('api-call.json'))
+    actual_api_call = JSON.parse(File.read('/tmp/api-call.json'))
 
     params.each do |k, v|
       assert actual_api_call.has_key?(k.to_s) && actual_api_call[k.to_s] == v
