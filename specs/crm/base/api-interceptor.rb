f7bc29c69567acf60803c5cc2219191d5f7ee24d
davidmz <david.mazvovsky@gmail.com>
2013-02-16 14:27:20 +0200

    non billale

--- specs/crm/base/api-interceptor.rb
+++ specs/crm/base/api-interceptor.rb
@@ -15,3 +15,13 @@ get '/api/email/mobile-site-client-information-request' do
   File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
   'OK'
 end
+
+get '/email/invalid-url' do
+  File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
+  'OK'
+end
+
+get '/email/not-the-right-person' do
+  File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
+  'OK'
+end
--- specs/crm/non-billable.rb
+++ specs/crm/non-billable.rb
@@ -6,5 +6,54 @@ require File.dirname(__FILE__) + '/base/lead.rb'
 require File.dirname(__FILE__) + '/base/crm_test_base.rb'
 
 class TestMini < CrmTestBase
+  def test_when_non_billable_and_reason_is_invalid_url_should_send_out_email_and_update_status_and_dates
+
+    email = "#{SecureRandom.uuid}@david.com"
+
+    lead = Lead.new driver, {
+        :email => "email #{email}",
+        :not_billable_c => 'check',
+        :non_billable_reason_c => 'select Invalid url'
+    }
+
+    assert_api_called({:email => email, :customerId => lead.id})
+
+    assert_includes lead.get('not_billable_assign_date_c'), today_crm_date
+    assert_includes lead.get('not_billable_assigner_c'), 'David MZ'
+    assert_includes lead.status, 'Pit stop'
+  end
+
+  def test_when_non_billable_and_reason_not_business_owner_should_send_out_email_and_update_status_and_dates
+
+    email = "#{SecureRandom.uuid}@david.com"
+
+    lead = Lead.new driver, {
+        :email => "email #{email}",
+        :not_billable_c => 'check',
+        :non_billable_reason_c => 'select Not business owner or decision maker'
+    }
+
+    assert_api_called({:email => email, :customerId => lead.id})
+
+    assert_includes lead.get('not_billable_assign_date_c'), today_crm_date
+    assert_includes lead.get('not_billable_assigner_c'), 'David MZ'
+    assert_includes lead.status, 'Dead'
+  end
+
+  def test_when_non_billable_and_reason_not_intrested_should_send_out_email_and_update_status_and_dates
+
+    email = "#{SecureRandom.uuid}@david.com"
+
+    lead = Lead.new driver, {
+        :email => "email #{email}",
+        :not_billable_c => 'check',
+        :non_billable_reason_c => 'select Not interested'
+    }
+
+    assert_includes lead.get('not_billable_assign_date_c'), today_crm_date
+    assert_includes lead.get('not_billable_assigner_c'), 'David MZ'
+    assert_includes lead.status, 'Dead'
+  end
+
 
 end
