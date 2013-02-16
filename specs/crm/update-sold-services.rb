5993563b1ccea0a0efc8db2d316c3019cf2090cd
davidmz <david.mazvovsky@gmail.com>
2013-02-16 02:50:36 +0200

    new tests

--- specs/crm/update-sold-services.rb
+++ specs/crm/update-sold-services.rb
@@ -12,10 +12,17 @@ class TestMini < CrmTestBase
     auth = Auth.new @driver
     auth.login
 
+    email = "#{SecureRandom.uuid}@david.com"
+
     lead = Lead.new @driver, {
         :status => 'select Client',
-        :mobileweb_check_c => 'check'
+        :mobileweb_check_c => 'check',
+        :email => "email #{email}"
     }
+
+    first_name = lead.get 'first_name'
+
+    assert_api_called({'email' => email, 'name' => first_name, 'customerId' => lead.id})
 
     assert_equal lead.get('mobileweb_info_req_sent_c'), today_crm_date
     assert_equal lead.get('mobileweb_sale_date_c'), today_crm_date
