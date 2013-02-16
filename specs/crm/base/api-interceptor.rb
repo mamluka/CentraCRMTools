bc3a2124baf0faaa0aa67ce4a29c71dc4e636126
davidmz <david.mazvovsky@gmail.com>
2013-02-16 02:57:36 +0200

    ulala

--- specs/crm/base/api-interceptor.rb
+++ specs/crm/base/api-interceptor.rb
@@ -5,3 +5,13 @@ get '/api/email/mobile-site-preview' do
   File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
   'OK'
 end
+
+get '/api/email/google-local-listing-client-information-request' do
+  File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
+  'OK'
+end
+
+get '/api/email/mobile-site-client-information-request' do
+  File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
+  'OK'
+end
