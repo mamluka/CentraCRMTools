a4b58d8e7a4a1d7b7fbcd90d4858523c73d3fb25
davidmz <david.mazvovsky@gmail.com>
2013-02-16 22:14:13 +0200

    yeah

---
+++ specs/jobs/spec-move-assigned-leads-to-system-pipeline.rb
@@ -0,0 +1,39 @@
+current_dir = File.dirname(__FILE__)
+
+require current_dir + "/base/jobs-base.rb"
+
+
+class MoveAssignedLeadsToSystemPipeline < JobsTestBase
+  def test_when_7_days_passed_from_assigned_status_should_move_to_system_pipeline
+
+    lead = lead_with do |lead|
+      lead.date_entered = 8.days.ago
+      lead.status = 'FU'
+    end
+
+    load_job 'move-assigned-leads-to-system-pipeline'
+
+    result = lead.reload
+
+    assert_equal result.status, "SP"
+    assert_equal result.assigned_user_id, $system_pipeline_user_id
+  end
+
+  def test_when_user_has_mobile_preview_url_and_7_days_passed_should_send_system_pipeline_emaail_one
+
+    lead = lead_with do |lead|
+      lead.date_entered = 8.days.ago
+      lead.status = 'FU'
+    end
+
+    lead.add_custom_data do |data|
+      data.prev_url_c = 'http://david.com'
+    end
+
+    load_job 'move-assigned-leads-to-system-pipeline'
+
+    result = lead.reload
+
+    assert result.system_pipeline_email_1_c > 5.minutes.ago
+  end
+end
