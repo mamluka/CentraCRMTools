46c2008ee8630cea3267b5e4dfab0500b159117a
davidmz <david.mazvovsky@gmail.com>
2013-02-16 18:49:29 +0200

    jobs start

--- specs/jobs/spec-cancellation-system-email.rb
+++ specs/jobs/spec-cancellation-system-email.rb
@@ -2,6 +2,8 @@ current_dir = File.dirname(__FILE__)
 
 require current_dir + "/base/jobs-base.rb"
 require current_dir + "/../../jobs/lib/init.rb"
+require current_dir + "/../../jobs/lib/active_records_models.rb"
+
 
 class CancellationSystemEmailTests < JobsTestBase
   def test_when_3_days_passed_from_cancellation_status_set_should_send_email
