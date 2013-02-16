21a36e18a0478d5402eff4a58651a4f5d8a0055d
davidmz <david.mazvovsky@gmail.com>
2013-02-16 16:12:48 +0200

    non billale

--- specs/crm/base/api-interceptor.rb
+++ specs/crm/base/api-interceptor.rb
@@ -1,27 +1,7 @@
 require 'sinatra'
 require 'json'
 
-get '/api/email/mobile-site-preview' do
-  File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
-  'OK'
-end
-
-get '/api/email/google-local-listing-client-information-request' do
-  File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
-  'OK'
-end
-
-get '/api/email/mobile-site-client-information-request' do
-  File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
-  'OK'
-end
-
-get '/email/invalid-url' do
-  File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
-  'OK'
-end
-
-get '/email/not-the-right-person' do
+get '/api/email/*' do
   File.open('api-call.json', 'w') { |file| file.write(params.to_json) }
   'OK'
 end
