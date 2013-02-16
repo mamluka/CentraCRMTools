d3f8fdf4bc848ab35a25ed295c89f4e54a4ad5a8
davidmz <david.mazvovsky@gmail.com>
2013-02-16 20:38:14 +0200

    yeah

--- specs/jobs/spec-cancellation-system-email.rb
+++ specs/jobs/spec-cancellation-system-email.rb
@@ -3,14 +3,14 @@ current_dir = File.dirname(__FILE__)
 require current_dir + "/base/jobs-base.rb"
 
 
-
 class CancellationSystemEmailTests < JobsTestBase
   def test_when_3_days_passed_from_cancellation_status_set_should_send_email
 
     lead = Lead.new
+    lead.status = 'cancelled'
     lead.save
 
-    lead.add_email "david@david.com"
+    lead.add_email "crmtesting@centracorporation.com"
 
     lead.add_custom_data do |data|
       data.cancellation_change_date_c = 4.days.ago
@@ -19,6 +19,5 @@ class CancellationSystemEmailTests < JobsTestBase
     lead.save
 
     load_job 'cancellation-system-email'
-
   end
 end
