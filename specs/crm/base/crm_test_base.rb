1cb0f595132dc07587c23db70974c593b9028c10
davidmz <david.mazvovsky@gmail.com>
2013-02-16 16:17:25 +0200

    non billale

--- specs/crm/base/crm_test_base.rb
+++ specs/crm/base/crm_test_base.rb
@@ -21,6 +21,8 @@ class CrmTestBase < MiniTest::Unit::TestCase
   def assert_api_called(params)
     actual_api_call = JSON.parse(File.read('api-call.json'))
 
+    puts actual_api_call
+
     params.each do |k, v|
       assert actual_api_call.has_key?(k.to_s) && actual_api_call[k.to_s] == v
     end
@@ -32,7 +34,7 @@ class CrmTestBase < MiniTest::Unit::TestCase
 
   def today_crm_time
     Time.now.strftime('%Y-%m-%d %H:%M')
-    end
+  end
 
   def today_crm_date
     Date.today.strftime('%m/%d/%Y')
