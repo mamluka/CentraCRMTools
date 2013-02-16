d506b6ad025648d011bc1320f17197c905b267e2
davidmz <david.mazvovsky@gmail.com>
2013-02-17 00:36:46 +0200

    yeah

--- specs/jobs/spec-move-cancellad-lead-to-system-pipeline.rb
+++ specs/jobs/spec-move-cancellad-lead-to-system-pipeline.rb
@@ -14,7 +14,7 @@ class Tests < JobsTestBase
 
     result = lead.reload
 
-    assert result.custom_data.cancellation_change_date_c > 5.minuets.ago
+    assert result.custom_data.cancellation_change_date_c > 5.minuts.ago
     assert_equal result.assigned_user_id, $system_pipeline_user_id
   end
 end
