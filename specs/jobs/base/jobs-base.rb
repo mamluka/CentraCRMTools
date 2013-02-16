008fc19e45efbcf44289587bb08b8aedc2567229
davidmz <david.mazvovsky@gmail.com>
2013-02-16 20:20:31 +0200

    yeah

--- specs/jobs/base/jobs-base.rb
+++ specs/jobs/base/jobs-base.rb
@@ -2,10 +2,20 @@ require 'minitest/autorun'
 require 'active_record'
 require 'active_support/all'
 
+current_dir = File.dirname(__FILE__)
+
+require current_dir + "/../../../jobs/lib/init.rb"
+require current_dir + "/../../../jobs/lib/active_records_models.rb"
+
 class JobsTestBase < MiniTest::Unit::TestCase
 
+  def setup
+    reload_database
+    ActiveRecord::Base.connection.execute("DELETE FROM leads;")
+  end
+
   def reload_database
-    current_dir = File.dirname(__FILE__)
+
     config = JSON.parse(File.read("#{current_dir}/database.json"))
 
     ActiveRecord::Base.establish_connection(
@@ -14,5 +24,9 @@ class JobsTestBase < MiniTest::Unit::TestCase
         :username => config['username'],
         :password => config['password'],
         :host => config['host'])
+  end
+
+  def load_job(job_file)
+    load current_dir + '/../../../jobs/' + job_file +'.rb'
   end
 end
--- specs/jobs/spec-cancellation-system-email.rb
+++ specs/jobs/spec-cancellation-system-email.rb
@@ -1,16 +1,13 @@
 current_dir = File.dirname(__FILE__)
 
 require current_dir + "/base/jobs-base.rb"
-require current_dir + "/../../jobs/lib/init.rb"
-require current_dir + "/../../jobs/lib/active_records_models.rb"
+
 
 
 class CancellationSystemEmailTests < JobsTestBase
   def test_when_3_days_passed_from_cancellation_status_set_should_send_email
-    reload_database
 
     lead = Lead.new
-    puts lead.id
     lead.save
 
     lead.add_email "david@david.com"
@@ -20,6 +17,8 @@ class CancellationSystemEmailTests < JobsTestBase
     end
 
     lead.save
+
+    load_job 'cancellation-system-email.rb'
 
   end
 end
