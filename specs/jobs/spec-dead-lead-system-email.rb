bca55f2f962f59e14520640f0828b6e534b28341
davidmz <david.mazvovsky@gmail.com>
2013-02-16 21:56:30 +0200

    yeah

--- specs/jobs/spec-dead-lead-system-email.rb
+++ specs/jobs/spec-dead-lead-system-email.rb
@@ -19,6 +19,43 @@ class DeadSystemEmailTests < JobsTestBase
 
     result = lead.reload
 
-    assert_includes result.custom_data.cancellation_email_sent_c.to_s, today_crm_time
+    assert_includes result.custom_data.dead_client_email_sent_c.to_s, today_crm_time
+  end
+
+  def test_when_4_days_passed_from_dead_status_set_should_not_send_email
+
+    lead = lead_with do |lead|
+      lead.status = 'Dead'
+      lead.assigned_user_id = $system_pipeline_user_id
+    end
+
+    lead.add_custom_data do |data|
+      data.dead_status_assigned_date_c = 4.days.ago
+    end
+
+    load_job 'dead-lead-system-email'
+
+    result = lead.reload
+
+    assert_nil result.custom_data.dead_client_email_sent_c.to_s
+  end
+
+  def test_when_dead_status_and_not_billable_should_not_send_email
+
+    lead = lead_with do |lead|
+      lead.status = 'Dead'
+      lead.assigned_user_id = $system_pipeline_user_id
+    end
+
+    lead.add_custom_data do |data|
+      data.dead_status_assigned_date_c = 4.days.ago
+      data.not_billable_c = true
+    end
+
+    load_job 'dead-lead-system-email'
+
+    result = lead.reload
+
+    assert_nil result.custom_data.dead_client_email_sent_c.to_s
   end
 end
