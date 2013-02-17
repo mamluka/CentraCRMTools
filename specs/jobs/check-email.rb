b682bc709ff4b8f957c63773753308275524351c
davidmz <david.mazvovsky@gmail.com>
2013-02-18 00:22:53 +0200

    yeah

---
+++ specs/jobs/check-email.rb
@@ -0,0 +1,13 @@
+require 'mail'
+
+Mail.defaults do
+  retriever_method :pop3, :address => "gator20.hostgator.com",
+                   :port => 995,
+                   :user_name => 'crmtesting@centracorporation.com',
+                   :password => '0953acb',
+                   :enable_ssl => true
+end
+
+mail.all.each do |mail|
+  puts mail.to
+end
