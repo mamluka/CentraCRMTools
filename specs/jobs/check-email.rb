dab4ab277c5fa3d74a2b190dc4f20a277830336c
davidmz <david.mazvovsky@gmail.com>
2013-02-18 20:27:13 +0200

    yeah

--- specs/jobs/check-email.rb
+++ specs/jobs/check-email.rb
@@ -9,10 +9,6 @@ Mail.defaults do
 end
 
 Mail.all.each do |mail|
-  puts mail.multipart?          #=> true
-  puts mail.parts.length        #=> 2
-  puts mail.parts.map { |p| p.content_type }  #=> ['text/plain', 'application/pdf']
-  puts mail.parts.map { |p| p.class }         #=> [Mail::Message, Mail::Message]
-  puts mail.parts[0].content_type_parameters  #=> {'charset' => 'ISO-8859-1'}
-  puts mail.parts[1].content_type_parameters  #=> {'name' => 'my.pdf'}
+  puts mail.parts[0].html_part  #=> {'charset' => 'ISO-8859-1'}
+  puts mail.parts[1].html_part  #=> {'name' => 'my.pdf'}
 end
