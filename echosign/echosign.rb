c820312a0bca59412dfac081c92b15a4b031b576
davidmz <david.mazvovsky@gmail.com>
2013-02-22 18:51:16 +0200

    adding grape

--- echosign/echosign.rb
+++ echosign/echosign.rb
@@ -1,25 +1,13 @@
-require 'savon'
-require 'json'
+require 'grape'
 
-config = JSON.parse File.read(File.dirname(__FILE__) + "/echosign.json")
+class EchoSignApi < Grape::Api
+  format :json
 
-API_KEY = config['apikey']
+  resource :echosign do
+    desc "Return a public timeline."
+    get :send do
+      "OK"
+    end
+  end
 
-client = Savon.client do
-  wsdl "https://centra.echosign.com/services/EchoSignDocumentService15?wsdl"
-  ssl_verify_mode :none
 end
-response = client.call :get_user_documents, :message => {:apiKey => API_KEY, :userCredentials => {email: config['email'], password: config['password']}}
-
-#puts response.body[:get_user_documents_response][:get_documents_for_user_result][:document_list_for_user][:document_list_item]
-
-#response.body[:get_user_documents_response][:get_documents_for_user_result][:document_list_for_user][:document_list_item].each do |doc|
-#  puts doc[:document_key]
-#end
-
-key = response.body[:get_user_documents_response][:get_documents_for_user_result][:document_list_for_user][:document_list_item].last[:document_key]
-puts key
-
-form_data = client.call :get_form_data, :message => {:apiKey => API_KEY, :documentKey => 'RWKWE73J68757D'}
-
-puts form_data.body
