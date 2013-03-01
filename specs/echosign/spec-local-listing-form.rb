625e2df0e62fde987eb64b22ce21c993ccfaff60
davidmz <david.mazvovsky@gmail.com>
2013-03-01 22:42:29 +0200

    refactor

--- specs/echosign/spec-local-listing-form.rb
+++ specs/echosign/spec-local-listing-form.rb
@@ -13,87 +13,25 @@ class LocalListingFormTests < EchoSignTestsBase
 
     @driver.goto contract_url
 
-    @driver.text_field(:name => 'contact_name').set 'David mz'
-    @driver.text_field(:name => 'contact_email').set 'david.mazvovsky@gmail.com'
-    @driver.text_field(:name => 'contact_phone').set '1234567890'
+    fill_basic_info
+    fill_billing_info
 
-    @driver.radio(:value => 'visa').set
+    not_same_billing_address
+    fill_billing_address
 
-    @driver.text_field(:name => 'billing_cc_number').set '1234567890'
-    @driver.text_field(:name => 'billing_cc_exp').set '03/16'
-    @driver.text_field(:name => 'billing_cc_ccv').set '123'
-    @driver.text_field(:name => 'billing_cc_name').set 'David mz card'
+    fill_client_details
+    fill_categories
+    fill_working_hours
+    fill_payment_types
 
-    @driver.radio(:value => 'not_same_address').set
-
-    @driver.text_field(:name => 'billing_address').set 'billing address'
-    @driver.text_field(:name => 'billing_city').set 'billing city'
-    @driver.select_list(:name => 'billing_state').select 'New York'
-    @driver.text_field(:name => 'billing_zip').set '12345'
-
-    @driver.text_field(:name => 'phone').set '1234567890'
-    @driver.text_field(:name => 'alt_phone').set '0987654321'
-    @driver.text_field(:name => 'name').set 'David the name'
-    @driver.text_field(:name => 'website').set 'http://www.centracorporation.com'
-    @driver.text_field(:name => 'email').set 'david@centracorporation.com'
-    @driver.text_field(:name => 'address').set 'address'
-    @driver.text_field(:name => 'city').set 'city'
-    @driver.select_list(:name => 'state').select 'Alabama'
-    @driver.text_field(:name => 'zip').set '54321'
-    @driver.text_field(:name => 'service_area').set 'LA'
-
-    @driver.text_field(:name => 'category1').set 'cat1'
-    @driver.text_field(:name => 'category2').set 'cat2'
-    @driver.text_field(:name => 'category3').set 'cat3'
-    @driver.text_field(:name => 'category4').set 'cat4'
-    @driver.text_field(:name => 'category5').set 'cat5'
-
-    @driver.text_field(:name => 'opening_hours_mon').set '1-2'
-    @driver.text_field(:name => 'opening_hours_tue').set '2-3'
-    @driver.text_field(:name => 'opening_hours_wed').set '3-4'
-    @driver.text_field(:name => 'opening_hours_thu').set '4-5'
-    @driver.text_field(:name => 'opening_hours_fri').set '5-6'
-    @driver.text_field(:name => 'opening_hours_sat').set '6-7'
-    @driver.text_field(:name => 'opening_hours_sun').set '7-8'
-
-    @driver.checkbox(:name => 'payment_type_check').set
-    @driver.checkbox(:name => 'payment_type_cash').set
-    @driver.checkbox(:name => 'payment_type_visa').set
-    @driver.checkbox(:name => 'payment_type_mastercard').set
-    @driver.checkbox(:name => 'payment_type_discover').set
-    @driver.checkbox(:name => 'payment_type_amex').set
-    @driver.checkbox(:name => 'payment_type_paypal').set
-    @driver.checkbox(:name => 'payment_type_google').set
-    @driver.checkbox(:name => 'payment_type_financing').set
-    @driver.checkbox(:name => 'payment_type_invoice').set
-    @driver.checkbox(:name => 'payment_type_diners').set
-
-    @driver.text_field(:name => 'company').set 'centra'
-    @driver.text_field(:name => 'print_name').set 'centra ltd'
-    @driver.text_field(:name => 'signature').set 'centra signature'
-    @driver.text_field(:name => 'title').set 'CTO'
-    @driver.text_field(:name => 'date').set '1/1/14'
-
-    @driver.execute_script("document.getElementById('document-frame').style.height='10000px'; document.getElementById('scrollable-wrapper').style.height='10000px'")
-
-    @driver.screenshot.save 'before.png'
-
-    @driver.element(:css => 'div[fieldname=echosign_signature] img').click
-
-    @driver.screenshot.save 'after.png'
-
-    @driver.text_field(:id => 'signature-name').when_present.set 'david mz'
-    @driver.div(:id => 'adopt').click
-
-    @driver.div(:id => 'submit').when_present.click
-
-    @driver.screenshot.save 'after-signed.png'
+    fill_sign_details
+    sign_form
 
     sleep 10
 
     assert_equal lead.get_list('billing_payment_method_c'), 'Visa'
 
-    #TODO checking the same billing checkbox is missing
+    assert lead.is_checked('billing_same_address_c')
 
     assert_equal lead.get('billing_address_street_c'), 'billing address'
     assert_equal lead.get('billing_address_city_c'), 'billing city'
@@ -136,5 +74,98 @@ class LocalListingFormTests < EchoSignTestsBase
     assert_includes lead.get('business_payment_types_c'), 'Diners'
 
     assert_includes lead.get('googlelisting_sign_date_c'), today_mysql_time
+  end
+
+  def sign_form
+    @driver.execute_script("document.getElementById('document-frame').style.height='10000px'; document.getElementById('scrollable-wrapper').style.height='10000px'")
+    @driver.element(:css => 'div[fieldname=echosign_signature] img').click
+    @driver.text_field(:id => 'signature-name').when_present.set 'david mz'
+    @driver.div(:id => 'adopt').click
+    @driver.div(:id => 'submit').when_present.click
+  end
+
+  def fill_sign_details
+    @driver.text_field(:name => 'company').set 'centra'
+    @driver.text_field(:name => 'print_name').set 'centra ltd'
+    @driver.text_field(:name => 'signature').set 'centra signature'
+    @driver.text_field(:name => 'title').set 'CTO'
+    @driver.text_field(:name => 'date').set '1/1/14'
+  end
+
+  def fill_payment_types
+    @driver.checkbox(:name => 'payment_type_check').set
+    @driver.checkbox(:name => 'payment_type_cash').set
+    @driver.checkbox(:name => 'payment_type_visa').set
+    @driver.checkbox(:name => 'payment_type_mastercard').set
+    @driver.checkbox(:name => 'payment_type_discover').set
+    @driver.checkbox(:name => 'payment_type_amex').set
+    @driver.checkbox(:name => 'payment_type_paypal').set
+    @driver.checkbox(:name => 'payment_type_google').set
+    @driver.checkbox(:name => 'payment_type_financing').set
+    @driver.checkbox(:name => 'payment_type_invoice').set
+    @driver.checkbox(:name => 'payment_type_diners').set
+  end
+
+  def fill_working_hours
+    @driver.text_field(:name => 'opening_hours_mon').set '1-2'
+    @driver.text_field(:name => 'opening_hours_tue').set '2-3'
+    @driver.text_field(:name => 'opening_hours_wed').set '3-4'
+    @driver.text_field(:name => 'opening_hours_thu').set '4-5'
+    @driver.text_field(:name => 'opening_hours_fri').set '5-6'
+    @driver.text_field(:name => 'opening_hours_sat').set '6-7'
+    @driver.text_field(:name => 'opening_hours_sun').set '7-8'
+  end
+
+  def fill_categories
+    @driver.text_field(:name => 'category1').set 'cat1'
+    @driver.text_field(:name => 'category2').set 'cat2'
+    @driver.text_field(:name => 'category3').set 'cat3'
+    @driver.text_field(:name => 'category4').set 'cat4'
+    @driver.text_field(:name => 'category5').set 'cat5'
+  end
+
+  def fill_client_details
+    @driver.text_field(:name => 'phone').set '1234567890'
+    @driver.text_field(:name => 'alt_phone').set '0987654321'
+    @driver.text_field(:name => 'name').set 'David the name'
+    @driver.text_field(:name => 'website').set 'http://www.centracorporation.com'
+    @driver.text_field(:name => 'email').set 'david@centracorporation.com'
+    @driver.text_field(:name => 'address').set 'address'
+    @driver.text_field(:name => 'city').set 'city'
+    @driver.select_list(:name => 'state').select 'Alabama'
+    @driver.text_field(:name => 'zip').set '54321'
+    @driver.text_field(:name => 'service_area').set 'LA'
+  end
+
+  def same_billing_address
+    @driver.radio(:value => 'same_address').set
+  end
+
+  def not_same_billing_address
+    @driver.radio(:value => 'same_address').set
+  end
+
+  def fill_billing_address
+
+
+    @driver.text_field(:name => 'billing_address').set 'billing address'
+    @driver.text_field(:name => 'billing_city').set 'billing city'
+    @driver.select_list(:name => 'billing_state').select 'New York'
+    @driver.text_field(:name => 'billing_zip').set '12345'
+  end
+
+  def fill_billing_info
+    @driver.radio(:value => 'visa').set
+
+    @driver.text_field(:name => 'billing_cc_number').set '1234567890'
+    @driver.text_field(:name => 'billing_cc_exp').set '03/16'
+    @driver.text_field(:name => 'billing_cc_ccv').set '123'
+    @driver.text_field(:name => 'billing_cc_name').set 'David mz card'
+  end
+
+  def fill_basic_info
+    @driver.text_field(:name => 'contact_name').set 'David mz'
+    @driver.text_field(:name => 'contact_email').set 'david.mazvovsky@gmail.com'
+    @driver.text_field(:name => 'contact_phone').set '1234567890'
   end
 end
