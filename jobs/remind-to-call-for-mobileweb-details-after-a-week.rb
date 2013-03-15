92d64c3296c341d7388feb7746767619b0a7de38
davidmz <david.mazvovsky@gmail.com>
2013-03-15 23:33:09 +0200

    added new job

---
+++ jobs/remind-to-call-for-mobileweb-details-after-a-week.rb
@@ -0,0 +1,30 @@
+require_relative "lib/jobs-base"
+
+class ResendMobileWebCustomerDataRequestJob < JobsBase
+
+  def execute
+    leads = CustomData.where('host_login_c is null and mobileweb_check_c = 1 and mobileweb_sale_date_c < ?', 7.days.ago).select { |x| !x.do_not_email }.map { |x| x.lead }.select { |x| x.status =="client" && x.emails.any? }
+    logger.info "Found #{leads.length.to_s} mobile web clients that did not enter their data for a whole week"
+
+    leads.each do |lead|
+      email = lead.email
+      logger.info "#{lead.name} will get an email to #{email}"
+
+      message = <<-EOS
+      Dear jordan #{lead.name} still did not fill in his details,
+
+      You need to call him to his #{lead.phone_work} and ask for the details
+      Here is a link to the crm http://crm.centracorporation.com/index.php?module=Leads&action=DetailView&record=#{lead.id}
+      EOS
+
+      mailer.local_listing_system_message "#{lead.name} still did not fill in his provider details for mobile web", message
+
+      sleep 5
+    end
+
+    logger.info "#{leads.length.to_s} mobile web clients got a request data reminder"
+  end
+end
+
+job = ResendMobileWebCustomerDataRequestJob.new
+job.execute
