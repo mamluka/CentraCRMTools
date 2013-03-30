0be755253f4803668eb908b5d389d9d19d81f0c1
davidmz <david.mazvovsky@gmail.com>
2013-03-30 13:34:51 +0300

    opps

--- specs/core/tests-base.rb
+++ specs/core/tests-base.rb
@@ -88,7 +88,7 @@ class TestsBase < MiniTest::Unit::TestCase
     #`screen -L -dmS echosign thin -p 9050 -P api.pid stop`
     puts "start killing"
 
-    'pkill -9 -f 9050'
+    `pkill -9 -f 9050`
     until `ps aux | grep 9050 | grep -v grep`.empty?
       sleep 0.1
     end
